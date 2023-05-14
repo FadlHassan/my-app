4489732b2f652daaeb65822d3a1de0dd75bf8ddd
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-05-14 18:15:58 +0100

    Created ArticleView component

--- src/components/views/article-view/ArticleView.jsx
+++ src/components/views/article-view/ArticleView.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import styles from './articleView.module.css';
 import HeaderBar from "../../header-bar/HeaderBar";
+import Typography from "../../typography/Typography";
+import TypographyVariants from "../../../data/typography_variants";
+import myImage from '../../../images/landing-page-girl.png';
 
 const ArticleView = () => {
     return (
         <div className={styles.articleView}>
             <HeaderBar/>
+            <div className={styles.content}>
+                <div className={styles.imageContainer}>
+                    <img src={myImage} alt="ArticleViewImage"/>
+                </div>
+                <div className={styles.titleDescription}>
+                    <Typography variant={TypographyVariants.HEADING_XL} color={styles.titleColor}>
+                        Title
+                    </Typography>
+                    <Typography variant={TypographyVariants.HEADING_M} color={styles.titleColor}>
+                        Subtitle
+                    </Typography>
+                </div>
+                <Typography variant={TypographyVariants.DESCRIPTION} className={styles.articleDescription}>
+                Too cultivated use solicitude frequently. Dashwood likewise up consider continue entrance ladyship oh. 
+                Wrong guest given purse power is no. Friendship to connection an am considered difficulty.
+                Among state cease how and sight since shall.
+                </Typography>
+            </div>
         </div>
     );
 };
--- src/data/typography_variants.jsx
+++ src/data/typography_variants.jsx
@@ -1,7 +1,9 @@
 const TypographyVariants = {
     HEADING_XL: "h1",
     HEADING_L: "h2",
-    HEADING_S: "h3"
+    HEADING_M: "h3",
+    HEADING_S: "h4",
+    DESCRIPTION: "description"
 };
 
 export default TypographyVariants;
