68df92c14c61f7236c37f06e73e0bfd69d3c8506
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-05-14 17:24:34 +0100

    Added image to ArticleCard.tsx

--- src/components/article-card/ArticleCard.jsx
+++ src/components/article-card/ArticleCard.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import styles from './articleCard.module.css';
 import Typography from "../typography/Typography";
 import TypographyVariants from "../../data/typography_variants";
+import myImage from '../../images/landing-page-girl.png';
+
 
 const ArticleCard = ({ title, subtitle }) => {
 
     return (
         <div className={styles.articleCard}>
+            <div className={styles.imageContainer}>
+                <img src={myImage} alt="ArticleCardImage"/>
+            </div>
             <div className={styles.titleDescription}>
                 <Typography variant={TypographyVariants.HEADING_L} color={styles.titleColor}>
                     {title}
