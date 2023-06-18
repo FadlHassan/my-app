a77a468e042da52cec01bca4f4e16c96654b5ba3
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-06-18 12:59:56 +0100

    Added hover state to header bar

--- src/components/header-bar/HeaderBar.jsx
+++ src/components/header-bar/HeaderBar.jsx
@@ -8,22 +8,26 @@ function HeaderBar() {
     return (
       <ul className={styles.header}>
         <li>
-          <NavLink to="/">
+          <NavLink className={styles.navLink}
+                   to="/">
             <Typography variant={TypographyVariants.HEADING_L} color={styles.color}>Home</Typography>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/blog">
+          <NavLink className={styles.navLink}
+                   to="/blog">
             <Typography variant={TypographyVariants.HEADING_L} color={styles.color}>Blog</Typography>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/contact">
+          <NavLink className={styles.navLink}
+                   to="/contact">
             <Typography variant={TypographyVariants.HEADING_L} color={styles.color}>Contact</Typography>
           </NavLink>
         </li>
         <li>
-        <NavLink to="/about">
+        <NavLink className={styles.navLink}
+                 to="/about">
           <Typography variant={TypographyVariants.HEADING_L} color={styles.color}>About</Typography>
         </NavLink>
         </li>
