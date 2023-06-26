0409b5ed9b07a1421be89c464e3a5c82b9569762
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-06-26 19:58:58 +0400

    Added typed animation

--- src/components/views/landing-view/LandingView.jsx
+++ src/components/views/landing-view/LandingView.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Typed from "react-typed";
 import TypographyVariants from "../../../data/typography_variants";
 import Typography from "../../typography/Typography";
 import HeaderBar from "../../header-bar/HeaderBar";
@@ -6,16 +7,27 @@ import styles from './landingView.module.css';
 import myImage from '../../../images/landing-page-girl.png';
 
 const LandingView = () => {
-    const title = 'Welcome, my name is Fadl Hassan';
+    const title = 'Welcome, my name is Fadl Hassan.';
 
     return (
         <div className={styles.landingView}>
             <HeaderBar/>
             <div className={styles.frame}>
                 <div className={styles.titleSection}>
-                    <Typography id="landingPageTitle" variant={TypographyVariants.HEADING_XL} color={styles.titleColor}>{title}</Typography>
+                    <Typography id="landingPageTitle" variant={TypographyVariants.HEADING_XL} color={styles.titleColor}>
+                        <Typed  
+                                showCursor
+                                strings={[title]}
+                                typeSpeed={100} 
+                                fadeOut
+                                onComplete={(self) => {
+                                    self.cursor.style.visibility = 'hidden';
+                                    self.cursor.style.opacity = '0';
+                                    self.cursor.style.transition = 'visibility 0s 1s, opacity 1s linear';
+                                }}/>
+                    </Typography>
                 </div>
-                <img class={styles.imageStyle} src={myImage} alt="Landing page"/>
+                <img className={styles.imageStyle} src={myImage} alt="Landing ppage"/>
             </div>
         </div>
         
