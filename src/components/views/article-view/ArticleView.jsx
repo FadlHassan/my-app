7fd226b75b0dbdcd90eaa838f5203e18ed04d1d6
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-07-09 21:50:50 +0100

    Add new color paletter

--- src/components/views/article-view/ArticleView.jsx
+++ src/components/views/article-view/ArticleView.jsx
@@ -13,19 +13,21 @@ const ArticleView = () => {
                 <div className={styles.imageContainer}>
                     <img src={myImage} alt="ArticleViewImage"/>
                 </div>
-                <div className={styles.titleDescription}>
-                    <Typography variant={TypographyVariants.HEADING_XL} color={styles.titleColor}>
-                        Title
-                    </Typography>
-                    <Typography variant={TypographyVariants.HEADING_M} color={styles.titleColor}>
-                        Subtitle
+                <div className={styles.articleContent}>
+                    <div className={styles.titleDescription}>
+                        <Typography variant={TypographyVariants.HEADING_XL} color={styles.titleColor}>
+                            Title
+                        </Typography>
+                        <Typography variant={TypographyVariants.HEADING_M} color={styles.titleColor}>
+                            Subtitle
+                        </Typography>
+                    </div>
+                    <Typography variant={TypographyVariants.DESCRIPTION} className={styles.articleDescription}>
+                    Too cultivated use solicitude frequently. Dashwood likewise up consider continue entrance ladyship oh. 
+                    Wrong guest given purse power is no. Friendship to connection an am considered difficulty.
+                    Among state cease how and sight since shall.
                     </Typography>
                 </div>
-                <Typography variant={TypographyVariants.DESCRIPTION} className={styles.articleDescription}>
-                Too cultivated use solicitude frequently. Dashwood likewise up consider continue entrance ladyship oh. 
-                Wrong guest given purse power is no. Friendship to connection an am considered difficulty.
-                Among state cease how and sight since shall.
-                </Typography>
             </div>
         </div>
     );
