a34a5a81f99bd52527ddfe30c831f1a6dc4797e1
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-08-20 18:46:38 +0100

    Moved styles to View

--- src/components/views/article-view/ArticleView.jsx
+++ src/components/views/article-view/ArticleView.jsx
@@ -3,30 +3,32 @@ import styles from './articleView.module.css';
 import Typography from "../../typography/Typography";
 import TypographyVariants from "../../../data/typography_variants";
 import myImage from '../../../images/landing-page-girl.png';
-import { useMediaQuery } from "react-responsive";
+import View from "../view/View";
 
 const ArticleView = () => {
     return (
-        <div className={styles.content}>
-            <div className={styles.imageContainer}>
-                <img src={myImage} alt="ArticleViewImage"/>
-            </div>
-            <div className={styles.articleContent}>
-                <div className={styles.titleDescription}>
-                    <Typography variant={TypographyVariants.HEADING_XL} color={styles.titleColor}>
-                        Title
-                    </Typography>
-                    <Typography variant={TypographyVariants.HEADING_M} color={styles.titleColor}>
-                        Subtitle
+        <View>
+            <div className={styles.content}>
+                <div className={styles.imageContainer}>
+                    <img src={myImage} alt="ArticleViewImage"/>
+                </div>
+                <div className={styles.articleContent}>
+                    <div className={styles.titleDescription}>
+                        <Typography variant={TypographyVariants.HEADING_XL} color={styles.titleColor}>
+                            Title
+                        </Typography>
+                        <Typography variant={TypographyVariants.HEADING_M} color={styles.titleColor}>
+                            Subtitle
+                        </Typography>
+                    </div>
+                    <Typography variant={TypographyVariants.DESCRIPTION} className={styles.articleDescription}>
+                    Too cultivated use solicitude frequently. Dashwood likewise up consider continue entrance ladyship oh. 
+                    Wrong guest given purse power is no. Friendship to connection an am considered difficulty.
+                    Among state cease how and sight since shall.
                     </Typography>
                 </div>
-                <Typography variant={TypographyVariants.DESCRIPTION} className={styles.articleDescription}>
-                Too cultivated use solicitude frequently. Dashwood likewise up consider continue entrance ladyship oh. 
-                Wrong guest given purse power is no. Friendship to connection an am considered difficulty.
-                Among state cease how and sight since shall.
-                </Typography>
             </div>
-        </div>
+        </View>
     );
 };
 
