2114bc6f0cf58654d04c39eaecb235997abddbe1
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-09-27 22:20:25 +0100

    Change to heroku url

--- src/services/article.service.js
+++ src/services/article.service.js
@@ -1,6 +1,6 @@
 export const getArticles = async () => {
     try {
-        const articles = await fetch('http://localhost:3001/api/articles').then(res => res.json()).then(data => data);
+        const articles = await fetch('https://fadlhassan-1bb6131358de.herokuapp.com/api/articles').then(res => res.json()).then(data => data);
         return articles;
     } catch (error) {
         console.log('Error:', error);
