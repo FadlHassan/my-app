3af7b2a81b44e0dbe42280654c4998d98b3feadc
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-10-12 17:32:58 +0100

    Add bounce animation

--- src/components/views/landing-view/LandingView.jsx
+++ src/components/views/landing-view/LandingView.jsx
@@ -6,8 +6,8 @@ import myImage from 'images/landing-page-girl.jpg';
 import { useMediaQuery } from 'react-responsive';
 import View from 'components/views/view/View';
 import placeholderImg from 'images/landing-page-girl-placeholder.jpg';
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import 'react-lazy-load-image-component/src/effects/blur.css';
+// import { LazyLoadImage } from "react-lazy-load-image-component";
+// import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const LandingView = ({style}) => {
     const isDesktopOrLarger = useMediaQuery({ minWidth: 1024 });
@@ -20,7 +20,9 @@ const LandingView = ({style}) => {
                     <Typography className={styles.title} id="landingPageTitle" variant={TypographyVariants.HEADING_L} color={styles.titleColor}>
                         Welcome, my name is <br/> Fadl Hassan <br/>
                     </Typography>
-                    <LazyLoadImage effect="blur" placeholderSrc={placeholderImg} src={myImage} alt="Landing page" className={styles.img}/>
+                    <div className={styles.imagePopup}>
+                        <img effect="blur" placeholderSrc={placeholderImg} src={myImage} alt="Landing page" className={styles.img}/>                        
+                    </div>
                 </div>  :
                 <div className={styles.mobileContent}>
                     <Typography className={styles.title} id="landingPageTitle" variant={TypographyVariants.HEADING_L} color={styles.titleColor}>
