087fabee180c152201d96113984dad0d218b2cb6
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-10-21 13:56:30 +0100

    Fix article card styles

--- src/data/typography_variants.jsx
+++ src/data/typography_variants.jsx
@@ -3,6 +3,7 @@ const TypographyVariants = {
     HEADING_L: "h2",
     HEADING_M: "h3",
     HEADING_S: "h4",
+    HEADING_XS: "h5",
     CONTACT_HEADER: "header",
     EMAIL: "email",
     DESCRIPTION: "description",
