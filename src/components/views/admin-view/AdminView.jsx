fd2a10a3906efb644c8ccfc7ee2c724590e6e386
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-11-18 11:08:45 +0000

    Create basic admin form

--- src/components/views/admin-view/AdminView.jsx
+++ src/components/views/admin-view/AdminView.jsx
@@ -1,14 +1,14 @@
 
 import React, { useState } from 'react';
+import { Box, TextField, Button} from '@mui/material';
 
 const AdminView = () =>{
     const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [image, setImage] = useState('');
+    const [subtitle, setSubtitle] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const article = { title, content, image };
+        const article = { title, subtitle };
         const response = await fetch('/api/articles', {
             method: 'POST',
             headers: {
@@ -17,31 +17,25 @@ const AdminView = () =>{
             body: JSON.stringify(article)
         });
         if (response.ok) {
-            // TODO: Handle successful submission
+            console.log('Successfuly created blog');
         } else {
-            // TODO: Handle submission error
+            console.log('Error in creating blog');
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Title:
-                <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
-            </label>
-            <br />
-            <label>
-                Content:
-                <textarea value={content} onChange={(event) => setContent(event.target.value)} />
-            </label>
-            <br />
-            <label>
-                Image:
-                <input type="text" value={image} onChange={(event) => setImage(event.target.value)} />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',
+        mt: 1, p: 3, gap: 5 }}>
+            <TextField sx={{ maxWidth: 500 }} label="Title" id="outlined-basic" onChange={(event) => setTitle(event.target.value)} />
+            <TextField sx={{ maxWidth: 500 }} label="Subtitle" id="outlined-basic" onChange={(event) => setSubtitle(event.target.value)} />
+            <Button
+                type="submit"
+                variant="contained"
+                sx={{ maxWidth: 100 }}
+            >
+                Submit
+            </Button>
+        </Box>
     );
 }
 
