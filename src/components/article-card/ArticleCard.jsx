60998f14958d3b71f0c306829cee14d6e7b88dcf
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-12-13 23:19:52 +0000

    Get all images and display them on BlogView

--- src/components/article-card/ArticleCard.jsx
+++ src/components/article-card/ArticleCard.jsx
@@ -1,4 +1,4 @@
-import React , { useState, useEffect } from 'react';
+import React from 'react';
 import styles from 'components/article-card/articleCard.module.css';
 import Typography from 'components/typography/Typography';
 import TypographyVariants from 'data/typography_variants';
@@ -6,13 +6,13 @@ import myImage from 'images/landing-page-girl.jpg';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 
-const ArticleCard = ({ article }) => {
+const ArticleCard = ({ article, imageUrl }) => {
     const isMobile = useMediaQuery({ maxWidth: 480 });
 
     return (
         <Link to={`/article/${article.id}`} className={styles.articleCard}>
             <div className={styles.imageContainer}>
-                <img src={myImage} alt="ArticleCardImage"/>
+                <img src={imageUrl || myImage} alt="ArticleCardImage"/>
             </div>
             <div className={styles.titleDescription}>
                 <Typography variant={isMobile ? TypographyVariants.HEADING_M : TypographyVariants.HEADING_L} color={styles.titleColor}>
