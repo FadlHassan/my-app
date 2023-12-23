24de6e440bc2750c78a4c211b5afbd530147898b
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-12-23 15:06:12 +0000

    Style form

--- src/components/views/admin-view/AdminView.jsx
+++ src/components/views/admin-view/AdminView.jsx
@@ -1,6 +1,5 @@
-
 import React, { useState } from 'react';
-import { Box, TextField, Typography, Button} from '@mui/material';
+import { Box, TextField, Typography, Button, Container} from '@mui/material';
 
 const AdminView = () =>{
     const [title, setTitle] = useState('');
@@ -89,69 +88,80 @@ const AdminView = () =>{
     }
 
     return (
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',
-        mt: 1, p: 3, gap: 5 }}>
-            <TextField sx={{ maxWidth: 500 }} label="Title" id="outlined-basic" onChange={(event) => setTitle(event.target.value)} />
-            <TextField sx={{ maxWidth: 500 }} label="Subtitle" id="outlined-basic" onChange={(event) => setSubtitle(event.target.value)} />
-            <Button
-                variant="contained"
-                component="label"
-                sx={{ mt: 2, mb: 2 }}
-            >
-                Upload Content PDF
-                <input
-                    type="file"
-                    hidden
-                    accept=".docx"
-                    onChange={handleDocChange}
-                />
-            </Button>
-            <Typography variant="body1">
-                    Content File uploaded: {fileName}
-            </Typography>
-    
-            <Button
-                type="submit"
-                onClick={handleSubmit}
-                variant="contained"
-                sx={{ maxWidth: 100 }}
-            >
-                Submit Content
-            </Button>
-            <Button
-                variant="contained"
-                component="label"
-                sx={{ mt: 2, mb: 2 }}
-            >
-                Upload Image
-                <input
-                    type="file"
-                    hidden
-                    accept=".jpg, .jpeg, .png"
-                    onChange={handleImageChange}
-                />
-            </Button>
-            <Typography variant="body1">
-                    Image File uploaded: {imageFileName}
-            </Typography>
-            <Button
-                type="submit"
-                onClick={handleSubmitImage}
-                variant="contained"
-                sx={{ maxWidth: 100 }}
-            >
-                Submit Image
-            </Button>                
-            <TextField sx={{ maxWidth: 500 }} label="Delete article" id="outlined-basic" onChange={(event) => setId(event.target.value)} />
-            <Button
-                type="submit"
-                onClick={handleDeleteArticle}
-                variant="contained"
-                sx={{ maxWidth: 100 }}
+        <Container maxWidth="lg" sx={{display: 'flex', flexDirection: 'row', gap: 10, justifyContent: 'center'}}>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',
+            mt: 1, p: 3, gap: 5 }}>
+                <Typography variant="h6" fontWeight='light' sx={{textDecoration: 'underline'}}>
+                        Create a new article
+                </Typography>
+                <TextField sx={{ maxWidth: 500 }} label="Title" id="outlined-basic" onChange={(event) => setTitle(event.target.value)} />
+                <TextField sx={{ maxWidth: 500 }} label="Subtitle" id="outlined-basic" onChange={(event) => setSubtitle(event.target.value)} />
+                <Typography variant="body1">
+                        Content File uploaded: {fileName}
+                </Typography>
+                <Button
+                    variant="contained"
+                    component="label"
+                    sx={{ mt: 2, mb: 2 }}
                 >
-                    Delete Article
-            </Button>        
-        </Box>
+                    Upload Content PDF
+                    <input
+                        type="file"
+                        hidden
+                        accept=".docx"
+                        onChange={handleDocChange}
+                    />
+                </Button>        
+                <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ mt: 2, mb: 2 }}
+                >
+                    Submit Content
+                </Button>
+            </Box>
+            <Box component="form" onSubmit={handleSubmitImage} noValidate sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 1, p: 3, gap: 5 }}>
+                <Typography variant="h6" fontWeight='light' sx={{textDecoration: 'underline'}}>
+                        Upload image for article
+                </Typography>
+                <Typography variant="body1">
+                        Image File uploaded: {imageFileName}
+                </Typography>
+                <Button
+                    variant="contained"
+                    component="label"
+                    sx={{ mt: 2, mb: 2 }}
+                >
+                    Upload Image
+                    <input
+                        type="file"
+                        hidden
+                        accept=".jpg, .jpeg, .png"
+                        onChange={handleImageChange}
+                    />
+                </Button>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ mt: 2, mb: 2 }}
+                >
+                    Submit Image
+                </Button>  
+            </Box>
+            <Box component="form" onSubmit={handleDeleteArticle} noValidate sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',mt: 1, p: 3, gap: 5 }}>   
+                <Typography variant="h6" fontWeight='light' sx={{textDecoration: 'underline'}}>
+                        Delete an article
+                </Typography>
+                <TextField sx={{ maxWidth: 500 }} label="Id" id="outlined-basic" onChange={(event) => setId(event.target.value)} />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ mt: 2, mb: 2 }}
+                    >
+                        Delete Article
+                </Button>   
+            </Box>     
+        </Container>
     );
 }
 
