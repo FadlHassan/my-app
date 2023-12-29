bc4128493491569b12147fbc696b85a09fb9f9df
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-12-29 19:18:19 +0000

    Add graphcms endpoint and retrieve article data

--- src/components/article-card/ArticleCard.jsx
+++ src/components/article-card/ArticleCard.jsx
@@ -7,23 +7,27 @@ import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 
 const ArticleCard = ({ article, imageUrl }) => {
-    const isMobile = useMediaQuery({ maxWidth: 480 });
+	const isMobile = useMediaQuery({ maxWidth: 480 });
 
-    return (
-        <Link to={`/article/${article.id}`} className={styles.articleCard}>
-            <div className={styles.imageContainer}>
-                <img src={imageUrl || myImage} alt="ArticleCardImage"/>
-            </div>
-            <div className={styles.titleDescription}>
-                <Typography variant={isMobile ? TypographyVariants.HEADING_M : TypographyVariants.HEADING_L} color={styles.titleColor}>
-                    {article.title}
-                </Typography>
-                <Typography variant={isMobile ? TypographyVariants.HEADING_XS : TypographyVariants.HEADING_S} color={styles.subtitleColor}>
-                    {article.subtitle}
-                </Typography>
-            </div>
-        </Link>
-    );
+	return (
+		<Link to={`/article/${article.id}`} className={styles.articleCard}>
+			<div className={styles.imageContainer}>
+				<img src={imageUrl || myImage} alt="ArticleCardImage" />
+			</div>
+			<div className={styles.titleDescription}>
+				<Typography
+					variant={
+						isMobile
+							? TypographyVariants.HEADING_M
+							: TypographyVariants.HEADING_L
+					}
+					color={styles.titleColor}
+				>
+					{article.title}
+				</Typography>
+			</div>
+		</Link>
+	);
 };
 
 export default ArticleCard;
--- src/services/article.service.js
+++ src/services/article.service.js
@@ -1,3 +1,5 @@
+import { GraphQLClient, gql } from 'graphql-request';
+
 export const getArticles = async () => {
     try {
         const articles = await fetch('https://fadlhassan-1bb6131358de.herokuapp.com/api/articles').then(res => res.json()).then(data => data);
@@ -37,3 +39,30 @@ export const loadAllImages = async () => {
     }
 }
 
+
+export const getArticles2 = async () => {
+    try {
+        const endpoint = process.env.REACT_APP_GRAPH_CMS_API_KEY;
+        console.log('Endpoint:', endpoint);
+        const graphQLClient = new GraphQLClient(endpoint,{
+            headers: {
+                authorization: `Bearer ${process.env.REACT_APP_GRAPH_CMS_AUTH_TOKEN}`,
+            }
+        });
+        const query = gql`
+            {
+                articles {
+                    title
+                    image {
+                        url
+                    }
+                }
+            }
+        `;
+        console.log('Query:', query);
+        const data = await graphQLClient.request(query);
+        return data.articles;
+    } catch (error) {
+        console.log('Error:', error);
+    }
+}
