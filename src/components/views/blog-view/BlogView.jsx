e0bb170907d2d20e19cc3b48e1c9b10b0d6fd0b1
FadlHassan <43733047+FadlHassan@users.noreply.github.com>
2023-12-29 19:18:19 +0000

    Add graphcms endpoint and retrieve article data

--- src/components/views/blog-view/BlogView.jsx
+++ src/components/views/blog-view/BlogView.jsx
@@ -1,40 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import styles from 'components/views/blog-view/blogView.module.css';
-import ArticleCard from "components/article-card/ArticleCard";
+import ArticleCard from 'components/article-card/ArticleCard';
 import View from 'components/views/view/View';
-import { getArticles, loadAllImages } from "services/article.service";
+import { getArticles2, loadAllImages } from 'services/article.service';
 
 const BlogView = () => {
-    const [articles, setArticles] = useState(null);
-    const [imageUrls, setImageUrls] = useState(null);
-    const [loading, setLoading] = useState(true);
+	const [articles, setArticles] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function fetchMyData() {
-            const result = await getArticles();
-            const imageUrls = await loadAllImages();
-            setArticles(result);
-            setImageUrls(imageUrls);
-            setLoading(false);
-        }
-        fetchMyData();
-      }, []);
+	useEffect(() => {
+		async function fetchMyData() {
+			const result = await getArticles2();
+			setArticles(result);
+			setLoading(false);
+		}
+		fetchMyData();
+	}, []);
 
-    if (loading) return <div>Loading...</div>;
+	if (loading) return <div>Loading...</div>;
 
-    return (
-        <View>
-            <div className={styles.content}>
-                {
-                    articles?.map((article, index) => {
-                        return (
-                            <ArticleCard key={index} article={article} imageUrl={imageUrls[article.id]}/>
-                        );
-                    })
-                }
-            </div>
-        </View>
-    );
+	console.log(articles);
+	return (
+		<View>
+			<div className={styles.content}>
+				{articles?.map((article, index) => {
+					return (
+						<ArticleCard
+							key={index}
+							article={article}
+							imageUrl={article.image.url}
+						/>
+					);
+				})}
+			</div>
+		</View>
+	);
 };
 
 export default BlogView;
